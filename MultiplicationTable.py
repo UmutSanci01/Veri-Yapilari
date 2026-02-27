9143ac0a6c598bc0b196d300b44cf687a95572a3
Umut-Umut <umutsanci25@gmail.com>
2026-02-27 14:46:44 +0300

    Multiplication Table solve added

---
+++ MultiplicationTable.py
@@ -0,0 +1,8 @@
+def print_multiplication_table(n):
+    for i in range(1, 11,):
+        print(f"{n} * {i} = {n * i}")
+
+
+if __name__ == "__main__":
+    print_multiplication_table(5)
+    print_multiplication_table(2)
