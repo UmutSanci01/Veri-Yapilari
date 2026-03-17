56e3e274d00256abd1d889bcbc5d4c50d80551e4
UmutSanci01 <umutsanci01@gmail.com>
2026-03-17 14:08:54 +0300

    nCr optimization

--- Medium Problems/ComputenCr.py
+++ Medium Problems/ComputenCr.py
@@ -4,14 +4,25 @@ def ncr(n, r):
     if r < 0: return 0
     if r > n: return 0
 
+    # kombinasyonun simetri kurali
+    r = min(r, n - r)
+
+    pay = 1
+    payda = 1
+
+    for i in range(r):
+        pay *= (n - i)
+        payda *= i + 1
     # kombinasyon işlemi her zaman tam sayı üretirmiş
-    return fact(n) // (fact(r) * fact(n - r)) 
+    # return fact(n) // (fact(r) * fact(n - r))
+    return pay // payda
 
 if __name__ == "__main__":
     test_nums = [
         (5, 2), # 10
         (2, 4), # 0
-        (5, 0) # 1
+        (5, 0), # 1
+        (100, 98)
     ]
     for n, r in test_nums:
         print(n, r, "nCr", ncr(n, r))
