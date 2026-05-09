c2c708dc8fa10ed6d7965c508a9385480d0d31a2
UmutSanci01 <umutsanci01@gmail.com>
2026-05-09 13:38:34 +0300

    Binary Tree, insert fonksiyonu ve Node sınıfı geliştirildi.

--- Data Structures/Tree/BinaryTree.py
+++ Data Structures/Tree/BinaryTree.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.rigt : Node = None
         self.left : Node = None
+        self.payloads : list[Node] = [data] # list of same datas
 
 class BinaryTree:
     def __init__(self):
@@ -17,12 +18,17 @@ class BinaryTree:
 
         temp = self.root        
         while temp:
-            if data >= temp.data: # right
+            if data > temp.data: # right
                 if temp.right:
                     temp = temp.right # continue from right
                 else: # temp.right == None
                     temp.right = node
                     break
+            elif data == temp.data: # list of same datas
+                # # Append data instead of the node object 
+                # because we no longer need the left and right pointers.
+                temp.payloads.append(data) 
+                break
             else: # left
                 if temp.left:
                     temp = temp.left # continue from left
