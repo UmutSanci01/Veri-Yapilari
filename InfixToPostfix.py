dc7e7ae7f654343708c10f24ef6de0356c4e8643
UmutSanci01 <umutsanci01@gmail.com>
2026-05-23 00:17:51 +0300

    infix to postfix tamamlandi

--- InfixToPostfix.py
+++ InfixToPostfix.py
@@ -1,29 +1,40 @@
-def infixToPostfix(infix : str) -> str:
-    operators = "*/+-)"
-
+def infixToPostfix(infix: str) -> str:
+    operators = {"+": 1, "-": 1, "*": 2, "/": 2}
     stack = []
-    result = ""
+    result = []
 
-    for c in infix:
-        if c == ')':
-            result += ' ' + (' '.join(stack))[::-1]
-            stack.clear()
-        elif len(stack) > 0 and operators.find(stack[-1]) <= operators.find(c):
-            result += ' ' + (' '.join(stack))[::-1]
-            stack.clear()
+    s = infix.replace(' ', '') # bosluklar temizlendi
+    for c in s:
+        if c == '(':
             stack.append(c)
+            
+        elif c == ')': # ac parantez gorunceye kadar stack'i sonuca bosalt
+            while stack and stack[-1] != '(':
+                result.append(stack.pop())
+            if stack: # ac parantezi cope at
+                stack.pop() 
+                
         elif c in operators:
+            # eger stack'in en tepesindeki operator mevcuttan buyuk veya esit oncelikteyse
+            # bu kosul saglandigi surece stack'i sonuca bosalt
+            while stack and stack[-1] in operators and operators[stack[-1]] >= operators[c]:
+                result.append(stack.pop())
+            
             stack.append(c)
-        elif c != '(':
-            result+=c
+            
+        else:
+            result.append(c)
 
-    result += ' ' + (' '.join(stack))[::-1]
+    # stack'de kalanlari da sonuca bosalt
+    while stack:
+        result.append(stack.pop())
 
-    return result
+    return " ".join(result)
 
-print(infixToPostfix("(A + B) * (C - D)"))
-print(infixToPostfix("A + B * C / D"))
-print(infixToPostfix("A + B"))
-print(infixToPostfix("A + B * C"))
-print(infixToPostfix("A - B - C"))
-print(infixToPostfix("A + B * C / D - E"))
+if __name__ == "__main__":
+    print(infixToPostfix("(A + B) * (C - D)"))
+    print(infixToPostfix("A + B * C / D"))
+    print(infixToPostfix("A + B"))
+    print(infixToPostfix("A + B * C"))
+    print(infixToPostfix("A - B - C"))
+    print(infixToPostfix("A + B * C / D - E"))
